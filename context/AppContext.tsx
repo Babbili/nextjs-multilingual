1f731678315bc5bc6c6624e4edcf86c1db929fac
Babbili <babbili@protonmail.com>
2022-06-28 10:04:12 +0400

    creat app context

---
+++ context/AppContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useContext } from 'react'
+// next/router  https://nextjs.org/docs/api-reference/next/router
+import { useRouter } from 'next/router'
+import en from '../locales/en.json'
+import fr from '../locales/fr.json'
+import { tType } from '../types'
+
+const router = useRouter()
+// import current locale from NextRouter
+const { locale } = router
+
+// assign t as either of the translation json files localted in '../locales' according to the current locale
+let t: tType = locale === 'en' ? en : fr
+
+// create app context to retrieve t value across the app 
+const Context = createContext({} as ReturnType<any>)
+
+// create context provider 
+export const ContextProvider: React.FC<any> = ({ children }) => {
+
+    return (
+      <Context.Provider value={t}>{children}</Context.Provider>
+    )
+}
+
+
+export function useAppContext() {
+  return useContext(Context)
+}
+
