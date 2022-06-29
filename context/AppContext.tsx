3167b8c6ada90d142ae1698f638632f8056851a2
Babbili <babbili@protonmail.com>
2022-06-29 11:30:12 +0400

    fix #1

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -5,26 +5,34 @@ import en from '../locales/en.json'
 import fr from '../locales/fr.json'
 import { tType } from '../types'
 
-const router = useRouter()
-// import current locale from NextRouter
-const { locale } = router
-
-// assign t as either of the translation json files localted in '../locales' according to the current locale
-let t: tType = locale === 'en' ? en : fr
-
-// create app context to retrieve t value across the app 
-const Context: React.Context<tType> = createContext<tType>(t )
 
 // create context provider 
 export const ContextProvider: React.FC<any> = ({ children }) => {
+  // import current locale from NextRouter
+  const { locale } = useRouter()
 
-    return (
-      <Context.Provider value={t}>{children}</Context.Provider>
-    )
+  // assign t as either of the translation json files localted in '../locales' according to the current locale
+  let t: tType = locale === 'en' ? en : fr
+  
+  // create app context to retrieve t value across the app 
+  const Context = createContext(t )
+
+  return (
+    <Context.Provider value={t}>{children}</Context.Provider>
+  )
 }
 
 
 export function useAppContext() {
+  // import current locale from NextRouter
+  const { locale } = useRouter()
+
+  // assign t as either of the translation json files localted in '../locales' according to the current locale
+  let t: tType = locale === 'en' ? en : fr
+  
+  // create app context to retrieve t value across the app 
+  const Context = createContext(t )
+  
   return useContext<tType>(Context)
 }
 
