6b4782eecbe67508f5e05ba130d6f7d6de04d941
Babbili <babbili@protonmail.com>
2022-07-15 14:09:44 +0400

    add & connect Mongodb

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
+//import NexPage type from Next
 import type { NextPage } from 'next'
+//import GetServerSideProps type from Next
 import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+// import Mongodb clientPromise
 import clientPromise from '../utils/dbConnect'
 import { useAppContext } from '../context/AppContext'
 import styles from '../styles/Home.module.css'
@@ -8,6 +11,7 @@ import { tType } from '../types'
 
 const Home: NextPage<{ data: tType}> = ({ data }) => {
 
+  // import t from app context
   const t = useAppContext()
 
   console.log('article', data.article)
@@ -26,20 +30,25 @@ const Home: NextPage<{ data: tType}> = ({ data }) => {
     </div>
   )
 }
-
 export default Home
 
 
-
+// Using GetServerSideProps method from NextJs to fetch app data from MongoDB
+// https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  // import current locale from context parameter
+  // https://nextjs.org/docs/api-reference/data-fetching/get-server-side-props#context-parameter
   const locale = context.locale || 'en'
+
+  // await MongoDB clientPromis
   const client = await clientPromise
-  const db = client.db('next')
+  const db = client.db('next') // call MongoDB database 'next'
+  // set appData according to the current locale to be one of the 2 collections in database 'en' or 'fr'
   const appData = await db.collection(locale).find().toArray()
   const data = appData[0].app_data
 
   return {
-    props: { data }
+    props: { data } // will be passed to the page component as a prop
   }
   
 }
--- utils/dbConnect.ts
+++ utils/dbConnect.ts
@@ -1,6 +1,7 @@
 import { MongoClient } from 'mongodb'
 
-const client = new MongoClient(process.env.MONGODB_URI , {})
+const client = new MongoClient(process.env.MONGODB_URI , {}) // please add your Mongo URI to .env file
 const clientPromise = client.connect()
 
+// Export a module-scoped MongoClient promise to be shared across functions
 export default clientPromise
