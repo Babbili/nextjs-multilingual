8e1fcb610f23d21ab15b8c680ecd18e26c9ee8a4
Babbili <babbili@protonmail.com>
2022-07-17 12:51:00 +0400

    fix process.env type

--- utils/dbConnect.ts
+++ utils/dbConnect.ts
@@ -1,6 +1,7 @@
 import { MongoClient } from 'mongodb'
 
-const client = new MongoClient(process.env.MONGODB_URI , {}) // please add your Mongo URI to .env file
+const MONGO_URI: string = process.env.MONGODB_URI!  // please add your Mongo URI to .env file
+const client = new MongoClient( MONGO_URI, {})
 const clientPromise = client.connect()
 
 // Export a module-scoped MongoClient promise to be shared across functions
